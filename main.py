18a15331ba2befc1c2f48114088dd050917495d4
tristan <tristanjakobi08@gmail.com>
2020-06-10 23:44:55 +0200

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+class vertex:
+
+	def __init__(self, x, y, facesList):
+		self.x = x
+		self.y = y
+		self.facesList = facesList
+
+
+class faces:
+	def __init__(self, aVertex, bVertex, cVertex):
+		self.aVertex = aVertex
+		self.bVertex = bVertex
+		self.cVertex = cVertex
+
+@staticmethod
+def lineOperation():
+	print("ff")
+
+
